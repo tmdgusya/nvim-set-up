0226e5348468b60b5fbef762ee89e0959dfcdfcd
tmdgusya <dev0jsh@gmail.com>
2024-04-29 16:52:40 +0900

    change theme to cyberbird, and all configurations about rose-pine are commented out

--- init.lua
+++ init.lua
@@ -142,7 +142,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   { 'numToStr/Comment.nvim', opts = {} },
-  { 'rose-pine/neovim', name = 'rose-pine' },
+  -- { 'rose-pine/neovim', name = 'rose-pine' },
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -155,20 +155,35 @@ require('lazy').setup({
       },
     },
   },
-  { -- You can easily change to a different colorscheme.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+  -- { -- You can easily change to a different colorscheme.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  {
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('cyberdream').setup {
+        -- Recommended - see "Configuring" below for more config options
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+        terminal_colors = true,
+      }
+      vim.cmd 'colorscheme cyberdream' -- set the colorscheme
     end,
   },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -223,74 +238,74 @@ require('lazy').setup({
   },
 })
 
-require('rose-pine').setup {
-  variant = 'auto', -- auto, main, moon, or dawn
-  dark_variant = 'main', -- main, moon, or dawn
-  dim_inactive_windows = false,
-  extend_background_behind_borders = true,
+-- require('rose-pine').setup {
+--   variant = 'auto', -- auto, main, moon, or dawn
+--   dark_variant = 'main', -- main, moon, or dawn
+--   dim_inactive_windows = false,
+--   extend_background_behind_borders = true,
+--
+--   enable = {
+--     terminal = true,
+--     legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+--     migrations = true, -- Handle deprecated options automatically
+--   },
+--
+--   styles = {
+--     bold = true,
+--     italic = true,
+--     transparency = false,
+--   },
+--
+--   groups = {
+--     border = 'muted',
+--     link = 'iris',
+--     panel = 'surface',
+--
+--     error = 'love',
+--     hint = 'iris',
+--     info = 'foam',
+--     note = 'pine',
+--     todo = 'rose',
+--     warn = 'gold',
+--
+--     git_add = 'foam',
+--     git_change = 'rose',
+--     git_delete = 'love',
+--     git_dirty = 'rose',
+--     git_ignore = 'muted',
+--     git_merge = 'iris',
+--     git_rename = 'pine',
+--     git_stage = 'iris',
+--     git_text = 'rose',
+--     git_untracked = 'subtle',
+--
+--     h1 = 'iris',
+--     h2 = 'foam',
+--     h3 = 'rose',
+--     h4 = 'gold',
+--     h5 = 'pine',
+--     h6 = 'foam',
+--   },
+--
+--   highlight_groups = {
+--     -- Comment = { fg = "foam" },
+--     -- VertSplit = { fg = "muted", bg = "muted" },
+--   },
+--
+--   before_highlight = function(group, highlight, palette)
+--     -- Disable all undercurls
+--     -- if highlight.undercurl then
+--     --     highlight.undercurl = false
+--     -- end
+--     --
+--     -- Change palette colour
+--     -- if highlight.fg == palette.pine then
+--     --     highlight.fg = palette.foam
+--     -- end
+--   end,
+-- }
 
-  enable = {
-    terminal = true,
-    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-    migrations = true, -- Handle deprecated options automatically
-  },
-
-  styles = {
-    bold = true,
-    italic = true,
-    transparency = false,
-  },
-
-  groups = {
-    border = 'muted',
-    link = 'iris',
-    panel = 'surface',
-
-    error = 'love',
-    hint = 'iris',
-    info = 'foam',
-    note = 'pine',
-    todo = 'rose',
-    warn = 'gold',
-
-    git_add = 'foam',
-    git_change = 'rose',
-    git_delete = 'love',
-    git_dirty = 'rose',
-    git_ignore = 'muted',
-    git_merge = 'iris',
-    git_rename = 'pine',
-    git_stage = 'iris',
-    git_text = 'rose',
-    git_untracked = 'subtle',
-
-    h1 = 'iris',
-    h2 = 'foam',
-    h3 = 'rose',
-    h4 = 'gold',
-    h5 = 'pine',
-    h6 = 'foam',
-  },
-
-  highlight_groups = {
-    -- Comment = { fg = "foam" },
-    -- VertSplit = { fg = "muted", bg = "muted" },
-  },
-
-  before_highlight = function(group, highlight, palette)
-    -- Disable all undercurls
-    -- if highlight.undercurl then
-    --     highlight.undercurl = false
-    -- end
-    --
-    -- Change palette colour
-    -- if highlight.fg == palette.pine then
-    --     highlight.fg = palette.foam
-    -- end
-  end,
-}
-
-vim.cmd 'colorscheme rose-pine'
+-- vim.cmd 'colorscheme rose-pine'
 -- vim.cmd("colorscheme rose-pine-main")
 -- vim.cmd("colorscheme rose-pine-moon")
 -- vim.cmd("colorscheme rose-pine-dawn")
